c04f72418414a7d8a9846d751d8c7f17215b5253
sebastianf232 <87022337+sebastianf232@users.noreply.github.com>
2022-03-09 23:43:28 -0600

    Create Factory.java

---
+++ Factory.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Hoja de Trabajo 4
+ * @author Sebastian Franco 21484
+ * @author Gabriela de Leon 
+ * Clase Factory
+ */
+public class Factory<E> {
+    private Pilas<E> stack;
+    public Pilas<E> getStack(){
+        Scanner scan = new Scanner(System.in);
+        boolean on = true;
+        int i = 0;
+        while (on){ ///Mientras este activa el Factory se hace el proceso. Se ingresa que tipo de stack o ista a usar.
+            System.out.println("Ingrese tipo de stack a utilziar: \n1.Vector \n2. ArrayList \n3. Lista (debera escoger tipo de lista)");
+            try{
+                i = scan.nextInt();
+            } catch (Exception e){
+                /// Se pone la Exception para obtener errores del usuario.
+                System.out.println("Ingrese solo 1, 2 o 3");
+                continue;
+            }
+            if (i <= 0 || i > 3){
+                /// Se pone la Exception para obtener errores del usuario.
+                System.out.println("Ingrese solo 1, 2 o 3");
+                continue;
+            }
+            break;
+        }
+
+        if(i == 1){ ///Stack de Pilas de Vector
+            stack = new StackPV<>();
+        } if (i == 2){ ///Stack de Pilas de ArrayList
+            stack = new StackPAL<>();
+        } if (i == 3){ ///Stack de Listas, donde tendra que escoger el tipo de lista.
+            stack = new StackPL<>();
+        }
+        return stack; ///Se regresa el Stack segun el orden que se escogio.
+    }
+}
