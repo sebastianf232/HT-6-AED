bad957ceac25a672e5319ed381826626c529fb3d
GabyArchila <87708508+GabyArchila@users.noreply.github.com>
2022-03-17 07:37:27 -0600

    Clase Cartas

---
+++ Cartas.java
@@ -0,0 +1,32 @@
+Public Class Cartas {
+    private int valor;
+    private String nombre;
+    private String tipo;
+
+    public Cartas(){}
+
+	public int getValor() {
+		return this.valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
