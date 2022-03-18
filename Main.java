75efeecd9e301ba92be3b52c285e063aeee836ed
GabyArchila <87708508+GabyArchila@users.noreply.github.com>
2022-03-17 19:23:18 -0600

    Main

---
+++ Main.java
@@ -0,0 +1,28 @@
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.ArrayList;
+//import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int op = sc.nextInt();
+        System.out.println("Implementacion que desea utilizar: "); 
+        System.out.println("1) HashMap"); 
+        System.out.println("2) TreeMap");
+        System.out.println("3) LinkedHashMap."); 
+            switch(op){
+                case 1:
+                System.out.println("HashMap"); 
+                case 2:
+                System.out.println(""); 
+                case 3:
+                System.out.println(""); 
+
+            }
+    }
+}
+}
